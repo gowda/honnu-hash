4e24a4d1b8b18ccc020284e60341d5850d845097
Basavanagowda Kanur <basavanagowda@gmail.com>
2011-10-25 02:49:40 +0530

    * hash-map   - seperate the hash-map meta node and data node   - implement a simple hash function (simple_hash_function)   - include an array of list_head's in hash-map meta node, to insert the     nodes based on hash value obtained through simple_hash_function

--- src/hash-map.c
+++ src/hash-map.c
@@ -5,13 +5,33 @@
 
 #include "hash-map.h"
 
+int simple_hash_function (char *str)
+{
+        int len = 0;
+        int idx = 0;
+        int sum = 0;
+        int hash = 0;
+
+        len = strlen (str);
+
+        for (idx = 0; idx < len; idx++)
+                sum += str[idx];
+
+        hash = sum % HASHMAX;
+
+        return hash;
+}
+
 hash_map_t *hash_map_init (void)
 {
         hash_map_t *head = NULL;
+        int idx = 0;
 
         head = calloc (1, sizeof (hash_map_t));
 
         INIT_LIST_HEAD (&head->nodes);
+        for (idx = 0; idx < HASHMAX; idx ++)
+                INIT_LIST_HEAD (&head->hashes[idx]);
 
         return head;
 }
@@ -21,8 +41,9 @@ hash_map_t *hash_map_set (hash_map_t *map,
                           char *key,
                           char *value)
 {
-        hash_map_t *node = NULL;
+        hash_node_t *node = NULL;
         char *old_value = NULL;
+        int hash = 0;
 
         old_value = hash_map_get (map, key);
         if (old_value) {
@@ -30,14 +51,18 @@ hash_map_t *hash_map_set (hash_map_t *map,
                 free (old_value);
         }
 
-        node = calloc (1, sizeof (hash_map_t));
+        node = calloc (1, sizeof (hash_node_t));
 
         INIT_LIST_HEAD (&node->nodes);
+        INIT_LIST_HEAD (&node->hash);
 
         node->key = strdup (key);
         node->value = strdup (value);
 
         list_add (&node->nodes, &map->nodes);
+
+        hash = simple_hash_function (key);
+        list_add (&node->hash, &map->hashes[hash]);
 
         return map;
 }
@@ -45,9 +70,12 @@ hash_map_t *hash_map_set (hash_map_t *map,
 char *hash_map_get (hash_map_t *map,
                     char *key)
 {
-        hash_map_t *node = NULL;
+        hash_node_t *node = NULL;
+        int hash = 0;
 
-        list_for_each_entry (node, &map->nodes, nodes) {
+        hash = simple_hash_function (key);
+
+        list_for_each_entry (node, &map->hashes[hash], hash) {
                 if (0 == strcmp (key, node->key))
                         return strdup (node->value);
         }
@@ -58,18 +86,27 @@ char *hash_map_get (hash_map_t *map,
 hash_map_t *hash_map_unset (hash_map_t *map,
                             char *key)
 {
-        hash_map_t *node = NULL, *tmp = NULL;
+        hash_node_t *node = NULL, *tmp = NULL;
+        int hash = 0;
 
-        list_for_each_entry_safe (node, tmp, &map->nodes, nodes) {
+        hash = simple_hash_function (key);
+
+
+        list_for_each_entry (node, &map->hashes[hash], hash) {
                 if (0 == strcmp (key, node->key)) {
-                        list_del_init (&node->nodes);
+                        tmp = node;
                         break;
                 }
         }
 
-        if (node) {
-                free (node->key);
-                free (node->value);
+        if (tmp) {
+                list_del_init (&tmp->nodes);
+                list_del_init (&tmp->hash);
+
+                free (tmp->key);
+                free (tmp->value);
+
+                free (tmp);
         }
 
         return map;
@@ -77,7 +114,7 @@ hash_map_t *hash_map_unset (hash_map_t *map,
 
 int hash_map_dump (hash_map_t *map)
 {
-        hash_map_t *node = NULL;
+        hash_node_t *node = NULL;
 
         list_for_each_entry (node, &map->nodes, nodes) {
                 printf ("key: %s\t value: %s\n", node->key, node->value);
--- src/hash-map.h
+++ src/hash-map.h
@@ -3,11 +3,19 @@
 
 #include "list.h"
 
+#define HASHMAX 1024
+
+typedef struct head {
+        struct list_head nodes;
+        struct list_head hashes[HASHMAX];
+} hash_map_t;
+
 typedef struct node {
         struct list_head nodes;
+        struct list_head hash;
         char *key;
         char *value;
-} hash_map_t;
+} hash_node_t;
 
 
 hash_map_t *hash_map_init (void);
